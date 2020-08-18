3ea958faf00d53ad05cd57ee929a8ffd70bda7f6
ziyili20 <lizy0820@gmail.com>
2020-08-18 13:56:50 -0400

    add R files

---
+++ R/compute_aic.R
@@ -0,0 +1,16 @@
+## compute aic to decide cell type number
+compute_aic <- function(estProp,Y.raw){
+
+     K = ncol(estProp)
+     Nsample = dim(Y.raw)[2]
+     idx = apply(estProp,2,function(x) sum(x) == 0)
+     estProp[,idx] = matrix(runif(Nsample*sum(idx),0.0001,0.0002),Nsample,sum(idx))
+     estProf <- t(csSAM::csfit(estProp, t(Y.raw))$ghat)
+     tmpmat <- estProf %*% t(estProp)
+     rss = norm(Y.raw-tmpmat,type = "F")^2
+     nSample = ncol(Y.raw) * nrow(Y.raw)
+     nParam = K*(nrow(Y.raw)+ncol(Y.raw))
+     aic = nSample*log(rss/nSample)+ 2*nParam + (2*nParam*(nParam+1))/(nSample-nParam-1)
+
+     return(aic)
+}
