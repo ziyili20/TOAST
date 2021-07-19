b3c90ad9029460bf8d0bfeb608119bcad90ee036
tlaus <t.lausova@dkfz.de>
2021-07-19 18:57:36 +0200

    add largeOK = TRUE to RefFreeCellMix_wrapper.R

--- R/RefFreeCellMix_wrapper.R
+++ R/RefFreeCellMix_wrapper.R
@@ -13,7 +13,8 @@ RefFreeCellMix_wrapper <- function(Y, K) {
     }
     outY <- RefFreeEWAS::RefFreeCellMix(Y,
            mu0=RefFreeEWAS::RefFreeCellMixInitialize(Y,
-                                                     K = K))
+                                                     K = K, 
+                                                     largeOK = TRUE))
     Prop0 <- outY$Omega
     return(Prop0)
 }
